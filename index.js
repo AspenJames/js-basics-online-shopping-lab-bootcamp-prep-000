6b2d8d3917080101b1ae7b4a892697583626b071
Aspen James <aspenjames@tqca.org>
2018-01-22 22:56:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber.length>0){
+  if(typeof(cardNumber)===int){
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     cart = []
   }else{
