b97ec5e7bf3e8c0bcca14b6fa884a6c839617d17
Aspen James <aspenjames@tqca.org>
2018-01-22 23:39:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,19 +75,19 @@ function removeFromCart(item) {
           slice.push(cart.slice(i+1, cart.length));
           cart = slice;
         }
-        return cart
+        return cart;
       }
     }
   }else{
-    console.log("That item is not in your cart.")
+    console.log("That item is not in your cart.");
   }
 }
 
 function placeOrder(cardNumber) {
   if(typeof(cardNumber)==='number'){
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    cart = []
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
   }else{
-    console.log("Sorry, we don't have a credit card on file for you.")
+    console.log("Sorry, we don't have a credit card on file for you.");
   }
 }
